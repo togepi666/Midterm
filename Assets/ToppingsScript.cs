29e4665fd5518900b7236a5f365e1459b59cb2ad
LAPTOP-KFHBV4V2\Edgar <egr274@nyu.edu>
2018-02-22 12:01:38 -0500

    Can now add cheese. Cheese is very small oops.

--- Assets/ToppingsScript.cs
+++ Assets/ToppingsScript.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 public class ToppingsScript : MonoBehaviour
 {
 	public GameObject pepperonis;
+
+	public GameObject cheeses;
 	// Use this for initialization
 	void Start () {
 		
@@ -23,5 +25,16 @@ public class ToppingsScript : MonoBehaviour
 		pep.transform.position = pickedUp.transform.position;
 		pep.transform.Translate(new Vector3(0,.24f,0));
 		pep.transform.SetParent(pickedUp.transform);
+		pickedUp.GetComponent<ManipulatePizza>().hasPepperoni = true;
+	}
+
+	public void addCheese()
+	{
+		GameObject pickedUp = GetComponent<characterMovement>().currentPickedup;
+		GameObject cheese = Instantiate(cheeses, new Vector3(0, 1, 0), Quaternion.identity);
+		cheese.transform.position = pickedUp.transform.position;
+		cheese.transform.Translate(new Vector3(0,.24f,0));
+		cheese.transform.SetParent(pickedUp.transform);
+		pickedUp.GetComponent<ManipulatePizza>().hasCheese = true;
 	}
 }
